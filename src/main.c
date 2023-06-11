fb7409afc22a5fe9acc6036dac744672f2983c41
Rener-Pontes <renercpontes@gmail.com>
2023-06-11 03:06:24 -0300

    Set up a basic main function to test if the functions implemented until now are working as expected.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "./lib/graph.h"
 
 int main() {
+	graph *grp = graphAlloc();
+	vertex *vert = NULL;
+	int n[] = {10, 20, 30, 40, 50},
+		*p = NULL;
 
+	for (int i = 0; i < 5; i++) 
+		graphInsertVertex(grp, &n[i]);
+	
+	vert = grp->first_vertex;
 
-	return 0;
+	for (int i = 0; i < 5; i++) {
+		p = (int*) vert->value;
+		printf("%d  ", *p);
+		vert = vert->next_vertex;
+	}
+	printf("\n");
+
+	graphRemoveVertex(grp, 3);
+	
+	vert = grp->first_vertex;
+
+	for (int i = 0; i < grp->vertex_amount; i++) {
+		p = (int*) vert->value;
+		printf("%d  ", *p);
+		vert = vert->next_vertex;
+	}
+	printf("\n");
+
 }
