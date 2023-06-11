d81fd63bc3b9240052577c605f43d449b9b44bb6
Rener-Pontes <renercpontes@gmail.com>
2023-06-10 23:19:44 -0300

    Create allocation and freeing functions. Implementation of the graphAlloc and graphFree functions, along as auxiliar functions like vertexFree and edgeFree.

---
+++ src/graph.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./lib/graph.h"
+
+graph  *graphAlloc() {
+	graph *grp = malloc(sizeof(graph));
+
+	assert(grp != NULL);
+
+	grp->first_vertex = NULL;
+	grp->vertex_amount = 0;
+
+	return grp;
+}
+
+void edgeFree(edge *first_edge) {
+	edge *ed = NULL, *aux_ed = NULL;
+
+	ed = first_edge;
+	while (ed) {
+		aux_ed = ed;
+		ed = ed->next_edge;
+		free(aux_ed);
+	}
+	
+}
+
+void vertexFree(vertex *first_vertex) {
+	vertex *vert = NULL, *aux_vert = NULL;
+
+	vert = first_vertex;
+	while (vert) {
+		edgeFree(vert->first_edge);
+		aux_vert = vert;
+		vert = vert->next_vertex;
+
+		free(aux_vert);
+	}
+	
+}
+
+void	graphFree(graph *grp) {
+	assert(grp != NULL);
+	vertexFree(grp->first_vertex);
+	free(grp);
+}
---
+++ src/lib/graph.h
@@ -0,0 +1,26 @@
+typedef struct graph graph;
+typedef struct vertex vertex;
+typedef struct edge edge;
+
+
+struct graph {
+	int 	vertex_amount;
+	vertex *first_vertex;
+};
+struct vertex {
+	void   *value;
+	int 	label;
+	int 	edges_amount;
+	vertex *next_vertex;
+	edge   *first_edge;
+};
+struct edge {
+	int 	label;
+	vertex *origin;
+	vertex *destiny;
+	edge   *next_edge;
+};
+
+
+graph  *graphAlloc();
+void	graphFree(graph *grp);
