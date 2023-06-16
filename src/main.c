9b0eaa69293c4b4aca7e93676f34758795bab076
yuri_venc <gamesclayman@gmail.com>
2023-06-16 10:41:17 -0300

    funcoes de insercao de usuarios adicao e remocao de seguidores

--- src/main.c
+++ src/main.c
@@ -22,6 +22,33 @@ void print(graph *grp) {
 	}
 }
 
+int cmp(void *a, void *b){
+	user *u1 = (user *)a;
+	user *u2 = (user *)b;
+}
+
+void userCreate(graph *grp){
+	vertex *v = NULL;
+	user newUser;
+	printf("NOme do novo usuário: ");
+	scanf("%s", newUser);
+	graphInsertVertex(grp, *newUser);
+}
+
+void userFollow(graph *grp, user first_user, user second_user){
+	int u1, u2;
+	u1 = graphFindVertexLabelByValue(grp, first_user);
+	u2 = graphFindVertexLabelByValue(grp, second_user);
+	graphInsertEdge(grp, u1, u2);
+}
+
+void userUnfollow(graph *grp, user first_user, user second_user){
+	int u1, u2;
+	u1 = graphFindVertexLabelByValue(grp, first_user);
+	u2 = graphFindVertexLabelByValue(grp, second_user);
+	graphRemoveEdge(grp, u1, u2);
+}
+
 int main() {
 	graph *grp = graphAlloc();
 	vertex *vert = NULL;
